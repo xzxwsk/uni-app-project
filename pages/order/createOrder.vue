3ca22a76a12bc825b8565914e64e34803ab9cd68
xzxwsk <xzxwsk@163.com>
2021-08-12 18:04:15 +0800

    修改订单中实际支付金额

--- pages/order/createOrder.vue
+++ pages/order/createOrder.vue
@@ -74,12 +74,13 @@
 				<view class="uni-list-cell" v-if="billObj.IsPay && billObj.IsBonus">
 					<view class="uni-list-cell-navigate">
 						<text class="item-title"><text>实际抵扣积分</text></text>
-						<input-box type="number" v-model="billObj.FactUseBonus" ref="FactUseBonus" placeholder="请输入实际抵扣积分" 
+						<input-box type="number" ref="FactUseBonus" placeholder="请输入实际抵扣积分" 
+							v-model="billObj.FactUseBonus"
 							@input="onBlurBonus"
 						></input-box>
 					</view>
 				</view>
-				<view class="uni-list-cell">
+				<view class="uni-list-cell" v-if="billObj.IsPay">
 					<view class="uni-list-cell-navigate">
 						<text class="item-title"><text>实际支付金额</text></text>
 						<text class="price" style="font-size: 40rpx;">{{actualAmount}}</text>
@@ -255,7 +256,8 @@
 						data.Items[0].Qty = num
 						data.Items[0].Amount = parseInt(data.Items[0].Price * data.Items[0].Qty * 100) / 100
 						data.Amount = data.Items[0].Amount
-						console.log('生成默认订单：', data);
+						this.actualAmount = data.Amount - data.FactUseBonus
+						console.log('订单详情：', data);
 						this.billObj = data;
 					});
 				} else {
@@ -282,6 +284,8 @@
 								data.FactUseBonus = data.Amount
 							}
 						}
+						this.actualAmount = data.Amount - data.FactUseBonus
+						console.log('actualAmount: ', this.actualAmount)
 						this.billObj = data;
 					});
 				}
@@ -407,7 +411,9 @@
 				}
 				this.actualAmount = this.billObj.Amount - this.billObj.FactUseBonus
 				this.actualAmount = Math.max(this.actualAmount, 0)
-				this.$refs.FactUseBonus.setValue(this.billObj.FactUseBonus);
+				if (this.billObj.IsBonus) {
+					this.$refs.FactUseBonus.setValue(this.billObj.FactUseBonus);
+				}
 			},
 			// 是否使用积分抵扣
 			onIsBonus(e) {
@@ -431,7 +437,7 @@
 					util.showToast({
 						title: `您的可用积分为${this.billObj.CanUseBonus}, 抵扣积分不能大于可用积分`
 					})
-				} else if (val > this.billObj.Amount) {
+				} else if (val > 0 && val > this.billObj.Amount) {
 					util.showToast({
 						title: `抵扣积分不能大于商品金额`
 					})

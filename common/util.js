f5b24eeab2cf636ef25c1d9d91b8b2b22d4baf13
xzxwsk <xzxwsk@163.com>
2021-08-23 18:14:59 +0800

    合并mini:帐户明细中的单据类型字段名称

--- common/util.js
+++ common/util.js
@@ -1,6 +1,6 @@
-const baseUrlGlobal = 'http://47.104.226.115/qcdm/'; // 开发 测试帐号：A0000002 密码：1234  15883670960 1234 appId: wxba5d4722564f6a06
-// const baseUrlGlobal = 'http://118.163.201.227/qcdm/'; // 生产环境
-// const baseUrlGlobal = 'https://www.zzxianchang.cn/qcdm/'; // 小程序 测试帐号：A0000002 密码：000000 appId: wxaf376d36626cae3a 
+// const baseUrlGlobal = 'http://47.104.226.115/qcdm/'; // 开发 测试帐号：A0000002 密码：1234  15883670960 1234 appId: wxba5d4722564f6a06
+// const baseUrlGlobal = 'http://118.163.201.227/qcdm/'; // 原生产环境
+const baseUrlGlobal = 'https://www.zzxianchang.cn/qcdm/'; // 小程序 测试帐号：A0000002 密码：000000 appId: wxaf376d36626cae3a 
 const updateUrl = 'http://www.zzxianchang.cn/appdownload/updatedata.json'; // 更新地址
 const imgUrl = 'http://rxfs.rtrh.net/qianchangyewu'; // 图片地址
 const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));

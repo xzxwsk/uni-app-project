0e53d76d67aba1ab4e586c1a9d6e511cd30fa3f9
xzxwsk <xzxwsk@163.com>
2021-09-11 00:51:54 +0800

    小程序引导页广告缓存

--- pages/importAd/importAd.vue
+++ pages/importAd/importAd.vue
@@ -31,22 +31,49 @@
 			}
 		},
 		created() {
-			this.st = setTimeout(() => {
-				util.goTab({
-					url: '../tabBar/index'
-				})
-			}, 6000)
+			// this.st = setTimeout(() => {
+			// 	util.goTab({
+			// 		url: '../tabBar/index'
+			// 	})
+			// }, 6000)
 			this.getAd()
 		},
 		methods: {
 			async getAd() {
+				const adImg = util.getStorageSync('importad')
+				if (adImg) {
+					this.img = adImg
+				}
 				let res = await util.ajax({
 					method: 'SYS.OptionsDAL.GetOptions',
 					params: {}
 				}).catch(() => {})
 				console.log('getAd: ', res)
 				if (res.data.result.PictureSplashFileName) {
-					this.img = util.getBaseUrl() + 'files/downloadfile?filename=' + res.data.result.PictureSplashFileName
+					const imgUrl = util.getBaseUrl() + 'files/downloadfile?filename=' + res.data.result.PictureSplashFileName
+					// this.img = util.getBaseUrl() + 'files/downloadfile?filename=' + res.data.result.PictureSplashFileName
+					// 下载图片，获得临时文件
+					uni.downloadFile({
+					    url: imgUrl,
+					    success: res => {
+					        if (res.statusCode === 200) {
+					            console.log('下载成功', res.tempFilePath)
+								// 保存
+								uni.saveFile({
+								    tempFilePath: res.tempFilePath,
+								    success: saveRes => {
+								        const savedFilePath = saveRes.savedFilePath
+										console.log('savedFilePath: ', savedFilePath)
+										// this.img = saveRes.savedFilePath
+										util.setStorageSync({
+											key: 'importad',
+											data: saveRes.savedFilePath
+										})
+								    }
+								})
+					        }
+					    }
+					})
 				}
 			},
 			onClickAd() {
